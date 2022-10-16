82f08f4454035664ccd589190dfcd41b490bd1aa
slimane-msb <slimanemesbah0@gmail.com>
2022-10-16 21:47:19 +0200

    list file init

---
+++ partie2/list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define ISNOT !=
+#define NOT !
+#define AND &&
+#define OR ||
+#define then
+typedef enum { false, true } bool;
+
+
+int main(){
+
+    return EXIT_SUCCESS; 
+}
